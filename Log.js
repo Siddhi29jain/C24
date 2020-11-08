57cdef6c7eaa25ed6167ad8c3ea6957a7d6ae861
Siddhi29jain <73173982+Siddhi29jain@users.noreply.github.com>
2020-11-08 11:23:33 +0530

    Update Log.js

--- Log.js
+++ Log.js
@@ -1,8 +1,8 @@
 class Log {
     constructor(x, y, height,angle) {
       var options = {
-          'restitution':0.8,
-          'friction':0.3,
+          'restitution':0.3,
+          'friction':1,
           'density':1.0
       }
       this.body = Bodies.rectangle(x, y, 20, height, options);
